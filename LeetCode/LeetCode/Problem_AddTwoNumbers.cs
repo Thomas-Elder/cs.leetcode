019fba5dd8af37da2552e6640716bddd8f5337cb
Thomas Elder <thombsaway@gmail.com>
2021-05-17 10:09:18 +1000

    Initial Commit

---
+++ LeetCode/LeetCode/Problem_AddTwoNumbers.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode
+{
+    public static class Problem_AddTwoNumbers
+    {
+
+        public static ListNode Add(ListNode l1, ListNode l2)
+        {
+            int a, b, c, remainder;
+            remainder = 0;
+
+            ListNode node = new ListNode();
+            ListNode head = node;
+
+            int i = 1;
+            while (l1 != null || l2 != null)
+            { 
+                Console.WriteLine("{0}", i++);
+
+                a = l1 != null ? l1.val : 0;
+                b = l2 != null ? l2.val : 0;
+                c = a + b + remainder;
+                remainder = 0;
+
+                if (c >= 10)
+                {
+                    remainder = c / 10;
+                    c = c - 10;
+                }
+
+                node.val = c;
+                node.next = new ListNode();
+                node = node.next;
+
+                l1 = l1 != null ? l1.next : null;
+                l2 = l2 != null ? l2.next : null;
+            }
+
+            if (remainder > 0)
+                node.val += remainder;
+
+            if (node.val == 0)
+                node = null;
+
+            return head;
+        }
+    }
+}
---
+++ LeetCode/LeetCode/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace LeetCode
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Leet Code Solutions!");
+
+            ListNode l1 = new ListNode(2);
+            l1.next = new ListNode(4);
+            l1.next.next = new ListNode(3);
+
+            ListNode l2 = new ListNode(5);
+            l2.next = new ListNode(6);
+            l2.next.next = new ListNode(4);
+
+            ListNode result = Problem_AddTwoNumbers.Add(l1, l2);
+
+            while (result != null)
+            {
+                Console.Write("{0}", result.val);
+                result = result.next;
+            }
+        }
+    }
+}

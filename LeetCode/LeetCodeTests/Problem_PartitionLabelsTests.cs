6b9af73f58062277de2e4978227ba299e94466a5
Thomas <thombsaway@gmail.com>
2021-05-17 20:27:04 +1000

    Set up first partitionLabels test

--- LeetCode/LeetCodeTests/Problem_PartitionLabelsTests.cs
+++ LeetCode/LeetCodeTests/Problem_PartitionLabelsTests.cs
@@ -1,5 +1,6 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using LeetCode;
+using System.Collections.Generic;
 
 namespace LeetCodeTests
 {
@@ -7,5 +8,20 @@ namespace LeetCodeTests
     public class Problem_PartitionLabelsTests
     {
 
+        [TestMethod]
+        public void PartitionLabels_WhenPassedStringOfSameChars_ReturnsListWithOneElement()
+        {
+            // Arrange
+            Problem_PartitionLabels problem_PartitionLabels = new Problem_PartitionLabels();
+            IList<int> expected = new List<int>(1);
+            expected.Add(4);
+            string s = "aaaa";
+
+            // Act
+            IList<int> actual = problem_PartitionLabels.PartitionLabels(s);
+
+            //Assert
+            Assert.AreEqual(expected.Count, actual.Count);
+        }
     }
 }

785915588970dfa6f1883f619963b863620f7092
Thomas Elder <thombsaway@gmail.com>
2021-05-17 13:18:47 +1000

    Update addTwoNumbers to only add a new node, if there's a next in either listNode. And add a new tail if the carry > 0. Getting the length correct now.

--- LeetCode/LeetCode/Problem_AddTwoNumbers.cs
+++ LeetCode/LeetCode/Problem_AddTwoNumbers.cs
@@ -9,41 +9,57 @@ namespace LeetCode
 
         public static ListNode Add(ListNode l1, ListNode l2)
         {
-            int a, b, c, remainder;
-            remainder = 0;
+            int a, b, c, carry;
+            carry = 0;
 
             ListNode node = new ListNode();
             ListNode head = node;
 
             int i = 1;
+
+            // While either list is not null, we still a number to handle
             while (l1 != null || l2 != null)
             { 
                 Console.WriteLine("{0}", i++);
 
+                // Get the value of this node, or use zero if it's null
                 a = l1 != null ? l1.val : 0;
                 b = l2 != null ? l2.val : 0;
-                c = a + b + remainder;
-                remainder = 0;
 
+                // Calculate the sum, including carries from the previous sum
+                c = a + b + carry;
+
+                // Reset the carry
+                carry = 0;
+
+                // If c is 10+, we need to carry 1 forward to the next node, and set c - 10
                 if (c >= 10)
                 {
-                    remainder = c / 10;
+                    carry = c / 10;
                     c = c - 10;
                 }
 
+                // Assign c to this node
                 node.val = c;
-                node.next = new ListNode();
-                node = node.next;
 
+                // If there's another node in either list, we'll need another result node
+                if (l1.next != null || l2.next != null)
+                {
+                    node.next = new ListNode();
+                    node = node.next;
+                }
+
+                // Set each list to the next node
                 l1 = l1 != null ? l1.next : null;
                 l2 = l2 != null ? l2.next : null;
             }
 
-            if (remainder > 0)
-                node.val += remainder;
-
-            if (node.val == 0)
-                node = null;
+            // Add any significant carry over to the tail
+            if (carry > 0)
+            {
+                node.next = new ListNode();
+                node.next.val = carry;
+            }
 
             return head;
         }
--- LeetCode/LeetCodeTests/Problem_AddTwoNumbersTests.cs
+++ LeetCode/LeetCodeTests/Problem_AddTwoNumbersTests.cs
@@ -59,5 +59,59 @@ namespace LeetCodeTests
                 actual = actual.next;
             }
         }
+
+        [TestMethod]
+        public void Add_WhenGivenTwoListNodesWithCarryOver_ReturnsLinkedListWithCorrectLength()
+        {
+            // Arrange
+            ListNode l1 = new ListNode(2);
+            l1.next = new ListNode(4);
+            l1.next.next = new ListNode(6);
+
+            ListNode l2 = new ListNode(5);
+            l2.next = new ListNode(6);
+            l2.next.next = new ListNode(4);
+
+            int expected = 4;
+            int actual = 0;
+
+            // Act
+            ListNode result = Problem_AddTwoNumbers.Add(l1, l2);
+
+            while (result != null)
+            {
+                actual++;
+                result = result.next;
+            }
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void Add_WhenGivenTwoListNodesWithCarryOver_ReturnsLinkedListWithCorrectValues()
+        {
+            // Arrange
+            ListNode l1 = new ListNode(2);
+            l1.next = new ListNode(4);
+            l1.next.next = new ListNode(6);
+
+            ListNode l2 = new ListNode(5);
+            l2.next = new ListNode(6);
+            l2.next.next = new ListNode(4);
+
+            int[] expected = { 7, 0, 1, 1 };
+            int i = 0;
+
+            // Act
+            ListNode actual = Problem_AddTwoNumbers.Add(l1, l2);
+
+            // Assert
+            while (actual != null)
+            {
+                Assert.AreEqual(expected[i++], actual.val);
+                actual = actual.next;
+            }
+        }
     }
 }

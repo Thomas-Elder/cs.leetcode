d8aa3e3f5765f83450923a88fe9cdb0feb4d0efa
Thomas <thombsaway@gmail.com>
2021-05-17 15:34:24 +1000

    Add additional tests with asymetrical lists.

--- LeetCode/LeetCodeTests/Problem_AddTwoNumbersTests.cs
+++ LeetCode/LeetCodeTests/Problem_AddTwoNumbersTests.cs
@@ -113,5 +113,81 @@ namespace LeetCodeTests
                 actual = actual.next;
             }
         }
+
+        [TestMethod]
+        public void Add_WhenGivenTwoListNodesOfDifferringLengths_ReturnsLinkedListWithCorrectLength()
+        {
+            // Arrange
+            ListNode l1 = new ListNode(2);
+            l1.next = new ListNode(4);
+
+            ListNode l2 = new ListNode(5);
+            l2.next = new ListNode(6);
+            l2.next.next = new ListNode(4);
+
+            int expected = 3;
+            int actual = 0;
+
+            // Act
+            ListNode result = Problem_AddTwoNumbers.Add(l1, l2);
+
+            while (result != null)
+            {
+                actual++;
+                result = result.next;
+            }
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void Add_WhenGivenTwoListNodesOfDifferringLengths_ReturnsLinkedListWithCorrectValues()
+        {
+            // Arrange
+            ListNode l1 = new ListNode(2);
+            l1.next = new ListNode(4);
+
+            ListNode l2 = new ListNode(5);
+            l2.next = new ListNode(6);
+            l2.next.next = new ListNode(4);
+
+            int[] expected = { 7, 0, 5 };
+            int i = 0;
+
+            // Act
+            ListNode actual = Problem_AddTwoNumbers.Add(l1, l2);
+
+            // Assert
+            while (actual != null)
+            {
+                Assert.AreEqual(expected[i++], actual.val);
+                actual = actual.next;
+            }
+        }
+
+        [TestMethod]
+        public void Add_WhenGivenTwoListNodesFromLCTests_ReturnsLinkedListWithCorrectValues()
+        {
+            // Arrange
+            ListNode l1 = new ListNode(9);
+
+            ListNode l2 = new ListNode(1);
+            l2.next = new ListNode(9);
+            l2.next.next = new ListNode(9);
+
+            int[] expected = { 0, 0, 0, 1 };
+            int i = 0;
+
+            // Act
+            ListNode actual = Problem_AddTwoNumbers.Add(l1, l2);
+
+            // Assert
+            while (actual != null)
+            {
+                Assert.AreEqual(expected[i++], actual.val);
+                actual = actual.next;
+            }
+        }
     }
 }

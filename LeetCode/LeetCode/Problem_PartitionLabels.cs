1f5b061a75b135dde068e557f9b96b0fb27fe160
Thomas <thombsaway@gmail.com>
2021-05-18 20:28:53 +1000

    Add new partition label tests and update partition label, passing leetcode now, not very snappy though!

--- LeetCode/LeetCode/Problem_PartitionLabels.cs
+++ LeetCode/LeetCode/Problem_PartitionLabels.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
 
 namespace LeetCode
 {
@@ -27,32 +28,68 @@ namespace LeetCode
         /// </summary>
         /// <param name="s"></param>
         /// <returns></returns>
-        public IList<int> PartitionLabels(string s)
+        public List<int> PartitionLabels(string s)
         {
             List<int> subStringLengths = new List<int>(500);
-            List<char> currentSubString = new List<char>(500);
-
-            char[] sCharArray = s.ToCharArray();
+             char[] sCharArray = s.ToCharArray();
 
             // For each character in the array
-            foreach(char c in sCharArray)
+            for (int i = 0; i < sCharArray.Length; i++)
             {
+                char c = sCharArray[i];
+                int start = i;
+                int end = 0;
 
-                // If it's not in the current substring... 
-                if (!currentSubString.Contains(c))
+                if (start == sCharArray.Length - 1)
+                    end = start;
+
+                // Find the last occurence of this character by running backward through
+                // the array from the last index.
+                for (int j = sCharArray.Length - 1; j > i; j--)
                 {
-                    // Add it in
-                    currentSubString.Add(c);
-                } else
-                {
-                    // Else add the currentSubString length to subStringLengths
-                    subStringLengths.Add(currentSubString.Count);
-                    currentSubString.Clear();
-                    currentSubString.Add(c);
+
+                    // If index j matches c, j is the new end of the string, break
+                    if (sCharArray[j] == c)
+                    {
+                        end = j;
+                        break;
+                    }
+                    // If we reach the i + 1st element, we've not found another c,
+                    // so c is it's own substring
+                    else if (j == i + 1)
+                    {
+                        end = i;
+                        break;
+                    }
                 }
-            }
+                
+                // Take the current subString
+                char [] subString = sCharArray.Skip(start).Take(end - start + 1).ToArray();
 
-            subStringLengths.Add(currentSubString.Count);
+                
+                // Now we've got a substring from c - last_c, but there are other chars in the
+                // string, we need to check if any of them are beyond end.
+                // We loop from end onward, and if any char beyond end is in the substring, we 
+                // set end to it's location.
+                for (int k = end; k < sCharArray.Length; k++)
+                {
+                    // If this substring contains k, set end to k, to include k in the subString
+                    if (subString.Contains(sCharArray[k]))
+                    {
+                        end = k;
+
+                        // Reset the subString because end has changed, because we want to check 
+                        // the next char against the full substring.
+                        subString = sCharArray.Skip(start).Take(end - start + 1).ToArray();
+                    }                    
+                }
+
+                // Find the new subString length and add it to the list
+                subStringLengths.Add(subString.Length);
+
+                // Set i to end, so we start a new substring.
+                i = end;
+            }
 
             return subStringLengths;
         }
--- LeetCode/LeetCodeTests/Problem_PartitionLabelsTests.cs
+++ LeetCode/LeetCodeTests/Problem_PartitionLabelsTests.cs
@@ -9,22 +9,19 @@ namespace LeetCodeTests
     {
 
         [TestMethod]
-        public void PartitionLabels_WhenPassedStringOfSameFourChars_ReturnsListWithFourElement()
+        public void PartitionLabels_WhenPassedStringOfSameFourChars_ReturnsListWithExpectedValue()
         {
             // Arrange
             Problem_PartitionLabels problem_PartitionLabels = new Problem_PartitionLabels();
-            IList<int> expected = new List<int>(4);
-            expected.Add(1);
-            expected.Add(1);
-            expected.Add(1);
-            expected.Add(1);
+            List<int> expected = new List<int>(4);
+            expected.Add(4);
             string s = "aaaa";
 
             // Act
-            IList<int> actual = problem_PartitionLabels.PartitionLabels(s);
+            List<int> actual = problem_PartitionLabels.PartitionLabels(s);
 
             //Assert
-            Assert.AreEqual(expected.Count, actual.Count);
+            CollectionAssert.AreEqual(expected, actual);
         }
 
         [TestMethod]
@@ -32,33 +29,89 @@ namespace LeetCodeTests
         {
             // Arrange
             Problem_PartitionLabels problem_PartitionLabels = new Problem_PartitionLabels();
-            IList<int> expected = new List<int>(1);
-            expected.Add(4);
+            List<int> expected = new List<int>(1);
+            expected.Add(1);
+            expected.Add(1);
+            expected.Add(1);
+            expected.Add(1);
             string s = "abcd";
 
             // Act
-            IList<int> actual = problem_PartitionLabels.PartitionLabels(s);
+            List<int> actual = problem_PartitionLabels.PartitionLabels(s);
 
             //Assert
             Assert.AreEqual(expected.Count, actual.Count);
         }
 
         [TestMethod]
-        public void PartitionLabels_WhenPassedStringFromTCExample_ReturnsListWithCorrectLengths()
+        public void PartitionLabels_WhenPassedStringFromTCExample_ReturnsListWithCorrectLength()
         {
             // Arrange
             Problem_PartitionLabels problem_PartitionLabels = new Problem_PartitionLabels();
-            IList<int> expected = new List<int>(500);
+            List<int> expected = new List<int>(500);
             expected.Add(9);
             expected.Add(7);
             expected.Add(8);
             string s = "ababcbacadefegdehijhklij";
 
             // Act
-            IList<int> actual = problem_PartitionLabels.PartitionLabels(s);
+            List<int> actual = problem_PartitionLabels.PartitionLabels(s);
 
             //Assert
             Assert.AreEqual(expected.Count, actual.Count);
+        }
+
+        [TestMethod]
+        public void PartitionLabels_WhenPassedStringFromTCExample_ReturnsListWithCorrectValues()
+        {
+            // Arrange
+            Problem_PartitionLabels problem_PartitionLabels = new Problem_PartitionLabels();
+            List<int> expected = new List<int>(500);
+            expected.Add(9);
+            expected.Add(7);
+            expected.Add(8);
+            string s = "ababcbacadefegdehijhklij";
+
+            // Act
+            List<int> actual = problem_PartitionLabels.PartitionLabels(s);
+
+            //Assert
+            CollectionAssert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void PartitionLabels_WhenPassedStringFromLCExample2_ReturnsListWithCorrectValues()
+        {
+            // Arrange
+            Problem_PartitionLabels problem_PartitionLabels = new Problem_PartitionLabels();
+            List<int> expected = new List<int>(500);
+            expected.Add(1);
+            expected.Add(9);
+            string s = "caedbdedda";
+
+            // Act
+            List<int> actual = problem_PartitionLabels.PartitionLabels(s);
+
+            //Assert
+            CollectionAssert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void PartitionLabels_WhenPassedStringFromLCExample3_ReturnsListWithCorrectValues()
+        {
+            // Arrange
+            Problem_PartitionLabels problem_PartitionLabels = new Problem_PartitionLabels();
+            List<int> expected = new List<int>(500);
+            expected.Add(13);
+            expected.Add(1);
+            expected.Add(1);
+            string s = "qiejxqfnqceocmy";
+
+            // Act
+            List<int> actual = problem_PartitionLabels.PartitionLabels(s);
+
+            //Assert
+            CollectionAssert.AreEqual(expected, actual);
         }
     }
 }

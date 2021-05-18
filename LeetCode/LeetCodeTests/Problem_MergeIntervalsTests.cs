7fbd15c130cd974cc90c122f8c9f06a0bec61be7
Thomas <thombsaway@gmail.com>
2021-05-18 20:38:00 +1000

    Stub out merge interval test class and one test case.

---
+++ LeetCode/LeetCodeTests/Problem_MergeIntervalsTests.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace LeetCodeTests
+{
+    [TestClass]
+    public class Problem_MergeIntervalsTests
+    {
+        [TestMethod]
+        public void Merge_WhenGivenTwoOverlappingIntervals_ReturnsOne()
+        {
+            // Arrange
+
+            // Act
+
+            // Assert
+
+        }
+    }
+}

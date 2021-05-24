ff20505d6f880c0432046b529a0a54e2f975b082
Thomas <thombsaway@gmail.com>
2021-05-24 21:31:17 +1000

    Add performance testing project to test the run times of problems and utility functions.

---
+++ LeetCode/PerformanceTesting/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace PerformanceTesting
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Performance testing LeetCode solutions.");
+            Console.WriteLine();
+
+            TrappingRainPerformance trp = new TrappingRainPerformance();
+            trp.TrappingRainWater2Performance();
+            trp.TrappingRainWater3Performance();
+            Console.ReadLine();
+        }
+    }
+}
---
+++ LeetCode/PerformanceTesting/TrappingRainPerformance.cs
@@ -0,0 +1,80 @@
+﻿using Problems;
+using System;
+using System.Diagnostics;
+
+namespace PerformanceTesting
+{
+    public class TrappingRainPerformance
+    {
+
+        public void TrappingRainWater2Performance()
+        {
+
+            Console.WriteLine("Testing TrappingRainWater2s");
+
+            // Arrange
+            TrappingRainWater2 trappingRainWater2 = new TrappingRainWater2();
+            Stopwatch sw = new Stopwatch();
+            int[] terrain = new int[981];
+            long averageRuntime = 0;
+            int numberOfRuns = 10;
+            int i;
+            int j = 0;
+
+            for (i = 980; i >= 0; i--)
+                terrain[j++] = i;
+
+            // Act
+            for (int k = 0; k < 10; k++)
+            {
+                sw.Reset();
+                sw.Start();
+                trappingRainWater2.Rainfall(terrain);
+                sw.Stop();
+                averageRuntime += sw.ElapsedMilliseconds;
+
+                Console.WriteLine("Runtime for trappingRainWater2.Rainfall run {0}: {1}", k, sw.ElapsedMilliseconds);
+            }
+
+            averageRuntime = averageRuntime / numberOfRuns;
+
+            // Assert
+            Console.WriteLine("Average runtime for trappingRainWater2.Rainfall: {0}", averageRuntime);
+        }
+
+        public void TrappingRainWater3Performance()
+        {
+
+            Console.WriteLine("Testing TrappingRainWater3");
+
+            // Arrange
+            TrappingRainWater3 trappingRainWater3 = new TrappingRainWater3();
+            Stopwatch sw = new Stopwatch();
+            int[] terrain = new int[981];
+            long averageRuntime = 0;
+            int numberOfRuns = 10;
+            int i;
+            int j = 0;
+
+            for (i = 980; i >= 0; i--)
+                terrain[j++] = i;
+
+            // Act
+            for (int k = 0; k < 10; k++)
+            {
+                sw.Reset();
+                sw.Start();
+                trappingRainWater3.Trap(terrain);
+                sw.Stop();
+                averageRuntime += sw.ElapsedMilliseconds;
+
+                Console.WriteLine("Runtime for trappingRainWater3.Rainfall run {0}: {1}", k, sw.ElapsedMilliseconds);
+            }
+
+            averageRuntime = averageRuntime / numberOfRuns;
+
+            // Assert
+            Console.WriteLine("Average runtime for trappingRainWater3.Rainfall: {0}", averageRuntime);
+        }
+    }
+}

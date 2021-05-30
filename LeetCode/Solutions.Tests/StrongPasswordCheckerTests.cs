63ea280a9a1742d7a72d4924f26b62c601e70869
Thomas <thombsaway@gmail.com>
2021-05-30 16:19:07 +1000

    Stub out new problem, strong password checker

---
+++ LeetCode/Solutions.Tests/StrongPasswordCheckerTests.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Solutions.Hard;
+
+namespace Solutions.Tests
+{
+    [TestClass]
+    public class StrongPasswordCheckerTests
+    {
+
+        [TestMethod]
+        public void StepsToChange_WhenGivenSingleCharacterString_Returns5()
+        {
+            // Arrange
+            StrongPasswordChecker strongPasswordChecker = new StrongPasswordChecker();
+            int expected = 5;
+            string password = "a";
+
+            // Act 
+            int actual = strongPasswordChecker.StepsToChange(password);
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void StepsToChange_WhenGiven3CharacterString_Returns3()
+        {
+            // Arrange
+            StrongPasswordChecker strongPasswordChecker = new StrongPasswordChecker();
+            int expected = 3;
+            string password = "aA1";
+
+            // Act 
+            int actual = strongPasswordChecker.StepsToChange(password);
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void StepsToChange_WhenGivenAStringString_Returns0()
+        {
+            // Arrange
+            StrongPasswordChecker strongPasswordChecker = new StrongPasswordChecker();
+            int expected = 0;
+            string password = "1337C0d3";
+
+            // Act 
+            int actual = strongPasswordChecker.StepsToChange(password);
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}

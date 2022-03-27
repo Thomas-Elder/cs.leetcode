1ccbf3121add5072211ceec3d2526b780d8d1d44
Thomas <thombsaway@gmail.com>
2022-03-28 10:46:34 +1100

    Added RomanToInteger class and initial tests based on leetcode examples.

---
+++ LeetCode/Solutions.Tests/Easy/RomanToIntegerTests.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Solutions.Easy;
+
+namespace Solutions.Tests.Easy
+{
+    [TestClass]
+    public class RomanToIntegerTests
+    {
+        private RomanToInteger _romanToInteger;
+
+        [TestMethod]
+        public void RomanToInt_WhenCalledWithLCExample_ReturnsCorrectResult()
+        {
+            // Arrange
+            var expected = 3;
+            var input = "III";
+
+            _romanToInteger = new RomanToInteger();
+
+            // Act
+            var result = _romanToInteger.RomanToInt(input);
+
+            // Assert
+            Assert.AreEqual(expected, result);
+        }
+
+        [TestMethod]
+        public void RomanToInt_WhenCalledWithSecondLCExample_ReturnsCorrectResult()
+        {
+            // Arrange
+            var expected = 58;
+            var input = "LVIII";
+
+            _romanToInteger = new RomanToInteger();
+
+            // Act
+            var result = _romanToInteger.RomanToInt(input);
+
+            // Assert
+            Assert.AreEqual(expected, result);
+        }
+
+        [TestMethod]
+        public void RomanToInt_WhenCalledWithThirdLCExample_ReturnsCorrectResult()
+        {
+            // Arrange
+            var expected = 1994;
+            var input = "MCMXCIV";
+
+            _romanToInteger = new RomanToInteger();
+
+            // Act
+            var result = _romanToInteger.RomanToInt(input);
+
+            // Assert
+            Assert.AreEqual(expected, result);
+        }
+    }
+}

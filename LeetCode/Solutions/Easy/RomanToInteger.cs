56766d27277dd1430398ea0fc8a9b840f15dba0c
Thomas <thombsaway@gmail.com>
2022-03-28 10:58:08 +1100

    Added naive solution, doesn't handle subtractions.

--- LeetCode/Solutions/Easy/RomanToInteger.cs
+++ LeetCode/Solutions/Easy/RomanToInteger.cs
@@ -20,9 +20,13 @@ namespace Solutions.Easy
     /// C             100
     /// D             500
     /// M             1000
-    /// For example, 2 is written as II in Roman numeral, just two one's added together. 12 is written as XII, which is simply X + II. The number 27 is written as XXVII, which is XX + V + II.
+    /// For example, 2 is written as II in Roman numeral, just two one's added together. 12 is written as XII, which is 
+    /// simply X + II. The number 27 is written as XXVII, which is XX + V + II.
     ///
-    /// Roman numerals are usually written largest to smallest from left to right.However, the numeral for four is not IIII. Instead, the number four is written as IV.Because the one is before the five we subtract it making four. The same principle applies to the number nine, which is written as IX.There are six instances where subtraction is used:
+    /// Roman numerals are usually written largest to smallest from left to right.However, the numeral for four is not IIII. 
+    /// Instead, the number four is written as IV.
+    /// Because the one is before the five we subtract it making four. The same principle applies to the number nine, 
+    /// which is written as IX.There are six instances where subtraction is used:
     ///
     /// I can be placed before V (5) and X(10) to make 4 and 9. 
     /// X can be placed before L(50) and C(100) to make 40 and 90. 
@@ -55,7 +59,29 @@ namespace Solutions.Easy
     {
         public int RomanToInt(string s)
         {
-            return 0;
+            Dictionary<char, int> map = new Dictionary<char, int>()
+            {
+                { 'I', 1 },
+                { 'V', 5 },
+                { 'X', 10 },
+                { 'L', 50 },
+                { 'C', 100 },
+                { 'D', 500 },
+                { 'M', 1000 }
+            };
+
+            // Parse string, catching leading I, X and D 
+            // Might need to track two characters at a time? This char, and the next char...
+
+            var charArray = s.ToCharArray();
+            var result = 0;
+
+            for (int i = 0, j = 1; i < charArray.Length; i++, j++)
+            {
+                result += map[charArray[i]];
+            }
+
+            return result;
         }
     }
 }

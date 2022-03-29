e23d2be81a9afe8aa11b62ce523994e8a15d08c7
Thomas <thombsaway@gmail.com>
2022-03-30 06:43:28 +1100

    Stub out mergetwosortedlists class and test

---
+++ LeetCode/Solutions.Tests/Easy/MergeTwoSortedListsTests.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Solutions.Easy;
+
+namespace Solutions.Tests.Easy
+{
+    [TestClass]
+    public class MergeTwoSortedListsTests
+    {
+
+        [TestMethod]
+        public void 
+    }
+}
---
+++ LeetCode/Solutions/Easy/MergeTwoSortedLists.cs
@@ -0,0 +1,12 @@
+﻿using Utility;
+
+namespace Solutions.Easy
+{
+    public class MergeTwoSortedLists
+    {
+        public ListNode MergeTwoLists(ListNode list1, ListNode list2)
+        {
+            return list1;
+        }
+    }
+}

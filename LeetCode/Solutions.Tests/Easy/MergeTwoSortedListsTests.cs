3d51daccfe367d5ed6020c7cc9a913ae418ef406
Thomas <thombsaway@gmail.com>
2022-03-30 07:36:10 +1100

    MergeTwoSortedLists passing leetcode.

--- LeetCode/Solutions.Tests/Easy/MergeTwoSortedListsTests.cs
+++ LeetCode/Solutions.Tests/Easy/MergeTwoSortedListsTests.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Utility;
 using Solutions.Easy;
 
 namespace Solutions.Tests.Easy
@@ -6,8 +7,104 @@ namespace Solutions.Tests.Easy
     [TestClass]
     public class MergeTwoSortedListsTests
     {
+        private readonly MergeTwoSortedLists _mergeTwoSortedLists;
 
+        public MergeTwoSortedListsTests()
+        {
+            _mergeTwoSortedLists = new MergeTwoSortedLists();
+        }
+
+        /// <summary>
+        /// Example 1:
+        /// Input: list1 = [1, 2, 4], list2 = [1, 3, 4]
+        /// Output: [1,1,2,3,4,4]
+        /// </summary>
         [TestMethod]
-        public void 
+        public void MergeTwoLists_WhenGivenFirstLeetCodeExample_ReturnsCorrectOutput()
+        {
+            // Arrange
+            ListNode l1 = new ListNode(1);
+            l1.next = new ListNode(2);
+            l1.next.next = new ListNode(4);
+
+            ListNode l2 = new ListNode(1);
+            l2.next = new ListNode(3);
+            l2.next.next = new ListNode(4);
+
+            // Act
+            var result = _mergeTwoSortedLists.MergeTwoLists(l1, l2);
+
+            // Assert
+            Assert.IsNotNull(result);
+            Assert.AreEqual(1, result.val);
+        }
+
+        /// <summary>
+        /// Example 2:
+        /// Input: list1 = [], list2 = []
+        /// Output: []
+        /// </summary>
+        [TestMethod]
+        public void MergeTwoLists_WhenGivenSecondLeetCodeExample_ReturnsCorrectOutput()
+        {
+            // Arrange
+            ListNode l1 = null;
+
+            ListNode l2 = null;
+
+            // Act
+            var result = _mergeTwoSortedLists.MergeTwoLists(l1, l2);
+
+            // Assert
+            Assert.IsNull(result);
+        }
+
+        /// <summary>
+        /// Example 3:
+        /// Input: list1 = [], list2 = [0]
+        /// Output: [0]
+        /// </summary>
+        [TestMethod]
+        public void MergeTwoLists_WhenGivenThirdLeetCodeExample_ReturnsCorrectOutput()
+        {
+            // Arrange
+            ListNode l1 = null;
+
+            ListNode l2 = new ListNode(0);
+
+            // Act
+            var result = _mergeTwoSortedLists.MergeTwoLists(l1, l2);
+
+            // Assert
+            Assert.IsNotNull(result);
+            Assert.AreEqual(0, result.val);
+        }
+
+        /// <summary>
+        /// Example 4:
+        /// Input: list1 = [-2,5], list2 = [-9,-6,-3,-1,1,6]
+        /// Output: [-9,-6,-3,-2,-1,1,5,6]
+        /// </summary>
+        [TestMethod]
+        public void MergeTwoLists_WhenGivenFourthLeetCodeExample_ReturnsCorrectOutput()
+        {
+            // Arrange
+            ListNode l1 = new ListNode(-2);
+            l1.next = new ListNode(5);
+
+            ListNode l2 = new ListNode(-9);
+            l2.next = new ListNode(-6);
+            l2.next.next = new ListNode(-3);
+            l2.next.next.next = new ListNode(-1);
+            l2.next.next.next.next = new ListNode(1);
+            l2.next.next.next.next.next = new ListNode(6);
+
+            // Act
+            var result = _mergeTwoSortedLists.MergeTwoLists(l1, l2);
+
+            // Assert
+            Assert.IsNotNull(result);
+            Assert.AreEqual(-9, result.val);
+        }
     }
 }
--- LeetCode/Solutions/Easy/MergeTwoSortedLists.cs
+++ LeetCode/Solutions/Easy/MergeTwoSortedLists.cs
@@ -2,11 +2,80 @@
 
 namespace Solutions.Easy
 {
+    /// <summary>
+    /// A class for solving Leetcode problem 21. Merge Two Sorted Lists
+    /// </summary>
+    /// You are given the heads of two sorted linked lists list1 and list2.
+    /// 
+    /// Merge the two lists in a one sorted list.The list should be made by splicing together the nodes of the first two lists.
+    /// 
+    /// Return the head of the merged linked list.
+    /// 
+    /// Example 1:
+    /// Input: list1 = [1, 2, 4], list2 = [1, 3, 4]
+    /// Output: [1,1,2,3,4,4]
+    /// 
+    /// Example 2:
+    /// Input: list1 = [], list2 = []
+    /// Output: []
+    /// 
+    /// Example 3:
+    /// Input: list1 = [], list2 = [0]
+    /// Output: [0]
+    /// 
+    /// Constraints:
+    /// The number of nodes in both lists is in the range [0, 50].
+    /// -100 <= Node.val <= 100
+    /// Both list1 and list2 are sorted in non-decreasing order.
+    /// 
     public class MergeTwoSortedLists
     {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="list1"></param>
+        /// <param name="list2"></param>
+        /// <returns></returns>
         public ListNode MergeTwoLists(ListNode list1, ListNode list2)
         {
-            return list1;
+            ListNode listResult = new ListNode();
+            ListNode head = listResult;
+
+            if (list1 == null && list2 == null)
+            {
+                return null;
+            }
+
+            while (list1 != null || list2 != null)
+            {
+                if (list1 is null)
+                {
+                    listResult.val = list2.val;
+                    list2 = list2.next;
+
+                } else if (list2 is null)
+                {
+                    listResult.val = list1.val;
+                    list1 = list1.next;
+
+                } else if (list1.val < list2.val)
+                {
+                    listResult.val = list1.val;
+                    list1 = list1.next;
+                } else
+                {
+                    listResult.val = list2.val;
+                    list2 = list2.next;
+                }
+
+                if (list1 != null || list2 != null)
+                {
+                    listResult.next = new ListNode();
+                    listResult = listResult.next;
+                }
+            }
+
+            return head;
         }
     }
 }

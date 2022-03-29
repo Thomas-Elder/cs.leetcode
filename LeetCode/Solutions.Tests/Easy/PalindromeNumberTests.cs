6cd489a9b9ee442695b1f64d494bd01cd2dc550e
Thomas <thombsaway@gmail.com>
2022-03-29 11:05:55 +1100

    Added PalindromeNumber and tests, passing leetcode.

---
+++ LeetCode/Solutions.Tests/Easy/PalindromeNumberTests.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Solutions.Easy;
+
+namespace Solutions.Tests.Easy
+{
+    [TestClass]
+    public class PalindromeNumberTests
+    {
+
+        private readonly PalindromeNumber _palindromeNumber;
+
+        public PalindromeNumberTests()
+        {
+            _palindromeNumber = new PalindromeNumber();
+        }
+
+        [TestMethod]
+        public void IsPalindrome_WhenGiven121_ReturnsTrue()
+        {
+            // Arrange
+            int input = 121;
+
+            // Act
+            bool result = _palindromeNumber.IsPalindrome(input);
+
+            // Assert
+            Assert.IsTrue(result);
+        }
+
+        [TestMethod]
+        public void IsPalindrome_WhenGivenNegative121_ReturnsFalse()
+        {
+            // Arrange
+            int input = -121;
+
+            // Act
+            bool result = _palindromeNumber.IsPalindrome(input);
+
+            // Assert
+            Assert.IsFalse(result);
+        }
+
+        [TestMethod]
+        public void IsPalindrome_WhenGiven10_ReturnsFalse()
+        {
+            // Arrange
+            int input = 10;
+
+            // Act
+            bool result = _palindromeNumber.IsPalindrome(input);
+
+            // Assert
+            Assert.IsFalse(result);
+        }
+    }
+}
---
+++ LeetCode/Solutions/Easy/PalindromeNumber.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Solutions.Easy
+{
+    /// <summary>
+    ///  class for solving Leetcode problem 9. Palindrome Number
+    /// </summary>
+    /// Given an integer x, return true if x is palindrome integer.
+    /// 
+    /// An integer is a palindrome when it reads the same backward as forward.
+    /// 
+    /// For example, 121 is a palindrome while 123 is not.
+    /// 
+    /// Example 1:
+    /// Input: x = 121
+    /// Output: true
+    /// Explanation: 121 reads as 121 from left to right and from right to left.
+    /// 
+    /// Example 2:
+    /// Input: x = -121
+    /// Output: false
+    /// Explanation: From left to right, it reads -121. From right to left, it becomes 121-. Therefore it is not a palindrome.
+    /// 
+    /// Example 3:
+    /// Input: x = 10
+    /// Output: false
+    /// Explanation: Reads 01 from right to left.Therefore it is not a palindrome.
+    /// 
+    /// Constraints:
+    /// 
+    /// -231 <= x <= 231 - 1
+    /// 
+    /// Follow up: Could you solve it without converting the integer to a string?
+    /// 
+    /// Passing on leetcode. Did not convert to string.
+    public class PalindromeNumber
+    {
+        public bool IsPalindrome(int x)
+        {
+            if (x < 0)
+            {
+                return false;
+            }
+
+            List <int> list = new List<int>();
+
+            while (x != 0)
+            {
+                list.Add(x % 10);
+                x /= 10;
+            }
+
+            // check for palindromeness
+            for (int i = 0, j = list.Count - 1; i < list.Count && j >=0; i++, j--)
+            {
+                if (list[i] != list[j])
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+    }
+}

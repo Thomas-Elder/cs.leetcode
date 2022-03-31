95d9caaf7ca0ad903c7d97c9bbc110131ad0c5f0
Thomas <thombsaway@gmail.com>
2022-03-31 20:04:57 +1100

    Stub out reverseInteger problem and test.

---
+++ LeetCode/Solutions.Tests/Medium/ReverseIntegerTests.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Solutions.Medium;
+
+namespace Solutions.Tests.Medium
+{
+    [TestClass]
+    public class ReverseIntegerTests
+    {
+        private readonly ReverseInteger _reverseInteger;
+
+        public ReverseIntegerTests()
+        {
+            _reverseInteger = new ReverseInteger();
+        }
+    }
+}
---
+++ LeetCode/Solutions/Medium/ReverseInteger.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Solutions.Medium
+{
+
+    /// <summary>
+    /// A class for solving LeetCode problem 7. Reverse Integer
+    /// </summary>
+    /// 
+    /// Given a signed 32-bit integer x, return x with its digits reversed.If reversing x causes the value 
+    /// to go outside the signed 32-bit integer range[-231, 231 - 1], then return 0.
+    /// 
+    /// Assume the environment does not allow you to store 64-bit integers(signed or unsigned).
+    /// 
+    /// Example 1:
+    /// Input: x = 123
+    /// Output: 321
+    /// 
+    /// Example 2:
+    /// Input: x = -123
+    /// Output: -321
+    /// 
+    /// Example 3:
+    /// Input: x = 120
+    /// Output: 21
+    ///  
+    /// Constraints:
+    /// -231 <= x <= 231 - 1
+    public class ReverseInteger
+    {
+
+        public int Reverse(int x)
+        {
+            return x;
+        }
+    }
+}

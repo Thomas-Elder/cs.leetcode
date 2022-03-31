743371385ba4d7d90fcd508a0248e38eeff90462
Thomas <thombsaway@gmail.com>
2022-03-31 20:07:17 +1100

    Added tests for 3 examples given.

--- LeetCode/Solutions.Tests/Medium/ReverseIntegerTests.cs
+++ LeetCode/Solutions.Tests/Medium/ReverseIntegerTests.cs
@@ -12,5 +12,47 @@ namespace Solutions.Tests.Medium
         {
             _reverseInteger = new ReverseInteger();
         }
+
+        [TestMethod]
+        public void Reverse_WhenCalledWithExample1_ReturnsCorrectResult()
+        {
+            // Arrange
+            var input = 123;
+            var expected = 321;
+
+            // Act
+            var result = _reverseInteger.Reverse(input);
+
+            // Assert
+            Assert.AreEqual(expected, result);
+        }
+
+        [TestMethod]
+        public void Reverse_WhenCalledWithExample2_ReturnsCorrectResult()
+        {
+            // Arrange
+            var input = -123;
+            var expected = -321;
+
+            // Act
+            var result = _reverseInteger.Reverse(input);
+
+            // Assert
+            Assert.AreEqual(expected, result);
+        }
+
+        [TestMethod]
+        public void Reverse_WhenCalledWithExample3_ReturnsCorrectResult()
+        {
+            // Arrange
+            var input = 120;
+            var expected = 21;
+
+            // Act
+            var result = _reverseInteger.Reverse(input);
+
+            // Assert
+            Assert.AreEqual(expected, result);
+        }
     }
 }

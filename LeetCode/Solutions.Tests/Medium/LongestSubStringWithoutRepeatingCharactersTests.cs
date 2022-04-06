17961f71a86e661d2494710a8851831ffe786e25
Thomas <thombsaway@gmail.com>
2022-04-06 16:53:55 +1000

    Stub out new medium leetcode problem.

---
+++ LeetCode/Solutions.Tests/Medium/LongestSubStringWithoutRepeatingCharactersTests.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Solutions.Medium;
+
+namespace Solutions.Tests.Medium
+{
+    [TestClass]
+    public class LongestSubStringWithoutRepeatingCharactersTests
+    {
+        private readonly LongestSubStringWithoutRepeatingCharacters _sut;
+
+        public LongestSubStringWithoutRepeatingCharactersTests()
+        {
+            _sut = new LongestSubStringWithoutRepeatingCharacters();
+        }
+
+        [TestMethod]
+        public void LengthOfLongestSubstring_WhenGivenExampleOne_ReturnsCorrectResult()
+        {
+            // Arrange
+            string input = "abcabcbb";
+            int expected = 3;
+
+            // Act 
+            var result = _sut.LengthOfLongestSubstring(input);
+
+            // Assert 
+            Assert.AreEqual(expected, result);
+        }
+
+        [TestMethod]
+        public void LengthOfLongestSubstring_WhenGivenExampleTwo_ReturnsCorrectResult()
+        {
+            // Arrange
+            string input = "bbbbb";
+            int expected = 1;
+
+            // Act 
+            var result = _sut.LengthOfLongestSubstring(input);
+
+            // Assert 
+            Assert.AreEqual(expected, result);
+        }
+
+        [TestMethod]
+        public void LengthOfLongestSubstring_WhenGivenExampleThree_ReturnsCorrectResult()
+        {
+            // Arrange
+            string input = "pwwkew";
+            int expected = 3;
+
+            // Act 
+            var result = _sut.LengthOfLongestSubstring(input);
+
+            // Assert 
+            Assert.AreEqual(expected, result);
+        }
+    }
+}

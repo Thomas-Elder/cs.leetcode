20500e5a3bc0a2672a8f4ae4e134c6489b2bf045
Thomas <thombsaway@gmail.com>
2022-05-16 21:07:55 +1000

    Stub out tests and class for leetcode problem 665

---
+++ LeetCode/Solutions.Tests/Medium/NonDecreasingArrayTests.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Solutions.Medium;
+using Utility;
+
+namespace Solutions.Tests.Medium
+{
+    [TestClass]
+    public  class NonDecreasingArrayTests
+    {
+        private readonly NonDecreasingArray _sut;
+
+        public NonDecreasingArrayTests()
+        {
+            _sut = new NonDecreasingArray();
+        }
+
+        [TestMethod]
+        public void CheckPossibility_WhenCalledWithExample1_ReturnsTrue()
+        {
+            // Arrange
+            var input = new int [] { 4, 2, 3 };
+
+            // Act
+            var result = _sut.CheckPossibility(input);
+
+            // Assert
+            Assert.IsTrue(result);
+        }
+
+        [TestMethod]
+        public void CheckPossibility_WhenCalledWithExample2_ReturnsFalse()
+        {
+            // Arrange
+            var input = new int[] { 4, 2, 1 };
+
+            // Act
+            var result = _sut.CheckPossibility(input);
+
+            // Assert
+            Assert.IsFalse(result);
+        }
+
+        [TestMethod]
+        public void CheckPossibility_WhenCalledWithExample3_ReturnsFalse()
+        {
+            // Arrange
+            var input = new int[] { 3, 4, 2, 3 };
+
+            // Act
+            var result = _sut.CheckPossibility(input);
+
+            // Assert
+            Assert.IsFalse(result);
+        }
+    }
+}
---
+++ LeetCode/Solutions/Medium/NonDecreasingArray.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Solutions.Medium
+{
+    /// <summary>
+    /// A class for solving LeetCode problem 665. Non-decreasing Array
+    /// </summary>
+    /// Given an array nums with n integers, your task is to check if it could become non-decreasing by modifying at most one element.
+    /// We define an array is non-decreasing if nums[i] <= nums[i + 1] holds for every i(0-based) such that(0 <= i <= n - 2).
+    /// 
+    /// Example 1:
+    /// Input: nums = [4,2,3]
+    /// Output: true
+    /// Explanation: You could modify the first 4 to 1 to get a non-decreasing array.
+    /// 
+    /// Example 2:
+    /// Input: nums = [4, 2, 1] 
+    /// Output: false 
+    /// Explanation: You can't get a non-decreasing array by modify at most one element.
+    /// 
+    /// Constraints:
+    /// n == nums.length
+    /// 1 <= n <= 104
+    /// -105 <= nums[i] <= 105
+    /// 
+    public class NonDecreasingArray
+    {
+
+        public bool CheckPossibility(int[] nums)
+        {
+            var changes = 0;
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                for (int j = i + 1; j < nums.Length; j++)
+                {
+                    if (nums[i] > nums[j])
+                    {
+                        changes++;
+                    }
+
+                    if (changes > 1)
+                    {
+                        return false;
+                    }
+                }
+            }
+
+            return true;
+        }
+    }
+}

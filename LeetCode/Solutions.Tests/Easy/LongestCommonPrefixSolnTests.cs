c06cfb66779b32c037ed6fbd6ca2d56daa591f37
Thomas <thombsaway@gmail.com>
2022-08-22 20:26:54 +1000

    Stubbing out easy leetcode problem and test class.

---
+++ LeetCode/Solutions.Tests/Easy/LongestCommonPrefixSolnTests.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Utility;
+using Solutions.Easy;
+
+namespace Solutions.Tests.Easy
+{
+    [TestClass]
+    public class LongestCommonPrefixSolnTests
+    {
+
+        private readonly LongestCommonPrefixSoln _sut;
+
+        public LongestCommonPrefixSolnTests()
+        {
+            _sut = new LongestCommonPrefixSoln();
+        }
+
+        [TestMethod]
+        public void LongestCommonPrefix_WhenCalledWithExample1_ReturnsCorrectOutput()
+        {
+            string[] input = { "flower", "flow", "flight" };
+            string expected = "fl";
+
+            var result = _sut.LongestCommonPrefix(input);
+
+            Assert.AreEqual(expected, result);
+        }
+
+        [TestMethod]
+        public void LongestCommonPrefix_WhenCalledWithExample2_ReturnsCorrectOutput()
+        {
+            string[] input = { "dog", "racecar", "car" };
+            string expected = "";
+
+            var result = _sut.LongestCommonPrefix(input);
+
+            Assert.AreEqual(expected, result);
+        }
+    }
+}
---
+++ LeetCode/Solutions/Easy/LongestCommonPrefixSoln.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Solutions.Easy
+{
+    /// <summary>
+    /// A class for solving Leetcode problem 14. Longest Common Prefix
+    /// </summary>
+    /// 
+    /// Write a function to find the longest common prefix string amongst an array of strings.
+    /// 
+    /// If there is no common prefix, return an empty string "".
+    /// 
+    /// Example 1:
+    /// Input: strs = ["flower", "flow", "flight"]
+    /// Output: "fl"
+    /// 
+    /// Example 2:
+    /// Input: strs = ["dog", "racecar", "car"]
+    /// Output: ""
+    /// Explanation: There is no common prefix among the input strings.
+    /// 
+    /// Constraints:
+    /// 1 <= strs.length <= 200
+    /// 0 <= strs[i].length <= 200
+    /// strs[i] consists of only lowercase English letters.
+    /// 
+    public class LongestCommonPrefixSoln
+    {
+        public string LongestCommonPrefix(string[] strs)
+        {
+            return "false";
+        }
+    }
+}

c4b29e7fa89f120e99327012130dd2af897d6e12
Thomas <thombsaway@gmail.com>
2022-09-18 16:25:14 +1000

    Leetcode problem 35.

---
+++ LeetCode/Solutions.Tests/Easy/SearchInsertPositionTests.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Solutions.Easy;
+
+namespace Solutions.Tests.Easy
+{
+    [TestClass]
+    public class SearchInsertPositionTests
+    {
+        private readonly SearchInsertPosition _searchInsertPosition;
+
+        public SearchInsertPositionTests()
+        {
+            _searchInsertPosition = new SearchInsertPosition();
+        }
+
+        [TestMethod]
+        public void SearchInsert_WhenCalledWithTargetAtFirstIndex_Returns0()
+        {
+            // Arrange
+            int [] nums = new int[] { 1, 2, 3 };
+            int target = 1;
+            int expected = 0;
+
+            // Act
+            int actual = _searchInsertPosition.SearchInsert(nums, target);
+            
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void SearchInsert_WhenCalledWithTargetShouldBeInsertedAt1_Returns1()
+        {
+            // Arrange
+            int[] nums = new int[] { 1, 3, 3 };
+            int target = 2;
+            int expected = 1;
+
+            // Act
+            int actual = _searchInsertPosition.SearchInsert(nums, target);
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
---
+++ LeetCode/Solutions/Easy/SearchInsertPosition.cs
@@ -0,0 +1,69 @@
+﻿using System; 
+
+namespace Solutions.Easy
+{
+    /// <summary>
+    /// A class for solving Leetcode problem 35. Search Insert Position
+    /// </summary>
+    /// Given a sorted array of distinct integers and a target value, return the index if the target is found. If not, return the index where it would be if it were inserted in order.
+    /// You must write an algorithm with O(log n) runtime complexity.
+    /// 
+    /// Example 1:
+    /// Input: nums = [1, 3, 5, 6], target = 5
+    /// Output: 2
+    /// 
+    /// Example 2:
+    /// Input: nums = [1, 3, 5, 6], target = 2
+    /// Output: 1
+    /// 
+    /// Example 3:
+    /// Input: nums = [1, 3, 5, 6], target = 7
+    /// Output: 4
+    /// 
+    /// Constraints:
+    /// 1 <= nums.length <= 104
+    /// -104 <= nums[i] <= 104
+    /// nums contains distinct values sorted in ascending order.
+    /// -104 <= target <= 104
+    public class SearchInsertPosition
+    {
+        /// <summary>
+        /// Given a sorted array of distinct integers and a target value, returns the index if the target is found.
+        /// If not, return the index where it would be if it were inserted in order.
+        /// </summary>
+        /// <param name="nums"></param>
+        /// <param name="target"></param>
+        /// <returns></returns>
+        public int SearchInsert(int[] nums, int target)
+        {
+            int start = 0;
+            int end = nums.Length - 1;
+
+            // The complexity restriction leads me to think we want to binary tree this somehow..
+            
+            while (start <= end)
+            {
+                int mid = (start + end) / 2;
+
+                // If the middle index is our target, return
+                if (nums[mid] == target)
+                {
+                    return mid;
+
+                // if mid is greater than the target, take the left sub array
+                } else if (nums[mid] > target)
+                {
+                    end = mid - 1;
+
+                // mid is less than the target, so take the right sub array
+                } else
+                {
+                    start = mid + 1;
+                }
+            }
+
+            // If we've made it here, end is the index below where we need to insert the target.
+            return end + 1;
+        }
+    }
+}

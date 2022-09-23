1e25a40c162248ee4920c88259347a971d55f30a
Thomas <thombsaway@gmail.com>
2022-09-23 15:53:53 +1000

    LengthOfLastWord implemented.

---
+++ LeetCode/Solutions.Tests/Easy/LengthOfLastWordTests.cs
@@ -0,0 +1,73 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Utility;
+using Solutions.Easy;
+
+namespace Solutions.Tests.Easy
+{
+    [TestClass]
+    public class LengthOfLastWordTests
+    {
+        private readonly LengthOfLastWord _lengthOfLastWord;
+
+        public LengthOfLastWordTests()
+        {
+            _lengthOfLastWord = new LengthOfLastWord();
+        }
+
+        [TestMethod]
+        public void LengthOfLastWordMethod_WhenPassedHelloWorld_Returns5()
+        {
+            // Arrange
+            string input = "Hello World";
+            int expected = 5;
+
+            // Act
+            int actual = _lengthOfLastWord.LengthOfLastWordMethod(input);
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void LengthOfLastWordMethod_WhenPassedAStringEndingInSpace_ReturnsCorrectLength()
+        {
+            // Arrange
+            string input = "Hello World ";
+            int expected = 5;
+
+            // Act
+            int actual = _lengthOfLastWord.LengthOfLastWordMethod(input);
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void LengthOfLastWordMethod_WhenPassedAStringEndingInSpaces_ReturnsCorrectLength()
+        {
+            // Arrange
+            string input = "Hello World     ";
+            int expected = 5;
+
+            // Act
+            int actual = _lengthOfLastWord.LengthOfLastWordMethod(input);
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void LengthOfLastWordMethod_WhenPassedAStringWithMoreThanTwoWords_ReturnsCorrectLength()
+        {
+            // Arrange
+            string input = "Hello is still worlds";
+            int expected = 6;
+
+            // Act
+            int actual = _lengthOfLastWord.LengthOfLastWordMethod(input);
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
---
+++ LeetCode/Solutions/Easy/LengthOfLastWord.cs
@@ -0,0 +1,61 @@
+﻿
+namespace Solutions.Easy
+{
+    /// <summary>
+    /// A class for solving leetcode problem 58. Length of Last Word
+    /// </summary>
+    /// Given a string s consisting of words and spaces, return the length of the last word in the string.
+    /// A word is a maximal substring consisting of non-space characters only.
+    /// 
+    /// Example 1:
+    /// Input: s = "Hello World"
+    /// Output: 5
+    /// Explanation: The last word is "World" with length 5.
+    /// 
+    /// Example 2:
+    /// Input: s = "   fly me   to   the moon  "
+    /// Output: 4
+    /// Explanation: The last word is "moon" with length 4.
+    /// 
+    /// Example 3:
+    /// Input: s = "luffy is still joyboy"
+    /// Output: 6
+    /// Explanation: The last word is "joyboy" with length 6.
+    /// 
+    /// Constraints:
+    /// 1 <= s.length <= 104
+    /// s consists of only English letters and spaces ' '.
+    /// There will be at least one word in s.
+    public class LengthOfLastWord
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="s"></param>
+        /// <returns></returns>
+        public int LengthOfLastWordMethod(string s)
+        {
+            int lastLength = 0;
+            int currentLength = 0;
+
+            foreach (char c in s)
+            {
+                if (c == ' ')
+                {
+                    lastLength = currentLength > 0 ? currentLength : lastLength;
+                    currentLength = 0;
+                } else
+                {
+                    currentLength++;
+                }
+            }
+
+            // If we've still got a currentLength, then the last character of the string
+            // is not a space, and therefore the currentLength is the length of the last 
+            // word in the string.
+            lastLength = currentLength > 0 ? currentLength : lastLength;
+
+            return lastLength;
+        }
+    }
+}

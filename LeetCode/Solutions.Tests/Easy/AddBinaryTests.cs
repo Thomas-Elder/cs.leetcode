aedf57a2f68c240a0ed4864d745774ef318f678a
Thomas <thombsaway@gmail.com>
2022-10-11 20:39:06 +1100

    AddBinary implementation, still failing one leetCode test.

---
+++ LeetCode/Solutions.Tests/Easy/AddBinaryTests.cs
@@ -0,0 +1,76 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Solutions.Easy;
+
+namespace Solutions.Tests.Easy
+{
+    [TestClass]
+    public class AddBinaryTests
+    {
+        private readonly AddBinary _addBinary;
+
+        public AddBinaryTests()
+        {
+            _addBinary = new AddBinary();
+        }
+
+        [TestMethod]
+        public void AddBinaryStrings_WhenGivenTwoZeroStrings_ReturnsZeroString()
+        {
+            // Arrange
+            string inputA = "0";
+            string inputB = "0";
+            string expected = "0";
+
+            // Act
+            string actual = _addBinary.AddBinaryStrings(inputA, inputB);
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void AddBinaryStrings_WhenGivenTwoStringsDifferentLengths_Returns4String()
+        {
+            // Arrange
+            string inputA = "11";
+            string inputB = "1";
+            string expected = "100";
+
+            // Act
+            string actual = _addBinary.AddBinaryStrings(inputA, inputB);
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void AddBinaryStrings_WhenGivenOneAndZeroStrings_ReturnsOneString()
+        {
+            // Arrange
+            string inputA = "0";
+            string inputB = "1";
+            string expected = "1";
+
+            // Act
+            string actual = _addBinary.AddBinaryStrings(inputA, inputB);
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void AddBinaryStrings_WhenGivenTwoOneStrings_ReturnsTwoString()
+        {
+            // Arrange
+            string inputA = "1";
+            string inputB = "1";
+            string expected = "10";
+
+            // Act
+            string actual = _addBinary.AddBinaryStrings(inputA, inputB);
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
---
+++ LeetCode/Solutions/Easy/AddBinary.cs
@@ -0,0 +1,77 @@
+﻿
+namespace Solutions.Easy
+{
+    /// <summary>
+    /// A class for solving leetcode problem 67. Add Binary
+    /// </summary>
+    /// Given two binary strings a and b, return their sum as a binary string.
+    /// 
+    /// Example 1:
+    /// Input: a = "11", b = "1"
+    /// Output: "100"
+    /// 
+    /// Example 2:
+    /// Input: a = "1010", b = "1011"
+    /// Output: "10101"
+    /// 
+    /// Constraints:
+    /// 1 <= a.length, b.length <= 104
+    /// a and b consist only of '0' or '1' characters.
+    /// Each string does not contain leading zeros except for the zero itself.
+    public class AddBinary
+    {
+        public string AddBinaryStrings(string a, string b)
+        {
+            // Trivial cases
+            if (a == b && a == "0")
+            {
+                return "0";
+            }
+
+            // Looping
+            // We need to start at the end of both strings, working backward
+            string result = "";
+            string carry = "0";
+
+            while (a.Length > 0 || b.Length > 0)
+            {
+
+                string currA = a.Substring(a.Length - 1);
+                a = a.Remove(a.Length - 1);
+
+                string currB = b.Substring(b.Length - 1);
+                b = b.Remove(b.Length - 1);
+
+                if (currA == currB && currA == "1")
+                {
+                    result = result.Insert(0, "0");
+                    carry = "1";
+                }
+
+                if (currA == currB && currA == "0")
+                {
+                    if (carry == "1")
+                    {
+                        result = result.Insert(0, "1");
+                        carry = "0";
+                    }
+
+                    result = result.Insert(0, "0");
+                }
+
+                if (currA != currB)
+                {
+                    result = result.Insert(0, "1");
+                    carry = "0";
+                }
+            }
+            
+            if (carry == "1")
+            {
+                result.Insert(0, "1");
+            }
+
+            return result;
+        }
+    }
+}

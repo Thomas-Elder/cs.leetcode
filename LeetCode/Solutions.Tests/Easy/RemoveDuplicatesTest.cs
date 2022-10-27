33ab423ea41aab6def7a7c50280e1a89ff8b6a7d
Thomas <thombsaway@gmail.com>
2022-10-27 20:27:08 +1100

    Implement leetcode problem 83 remove duplicates from sorted linked list

---
+++ LeetCode/Solutions.Tests/Easy/RemoveDuplicatesTest.cs
@@ -0,0 +1,86 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Solutions.Easy;
+using Utility;
+
+namespace Solutions.Tests.Easy
+{
+    [TestClass]
+    public class RemoveDuplicatesTest
+    {
+        private readonly RemoveDuplicates _removeDuplicates;
+
+        public RemoveDuplicatesTest()
+        {
+            _removeDuplicates = new RemoveDuplicates();
+        }
+
+        [TestMethod]
+        public void DeleteDuplicates_WhenPassedSingleNode_ReturnsSingleNode()
+        {
+            // Arrange
+            ListNode head = new ListNode(1);
+
+            ListNode expected = new ListNode(1);
+
+            // Act
+            var actual = _removeDuplicates.DeleteDuplicates(head);
+
+            // Assert
+            Assert.AreEqual(expected.val, actual.val);
+        }
+
+        [TestMethod]
+        public void DeleteDuplicates_WhenPassedTwoIdenticalNodes_ReturnsSingleNodeAndNextIsNull()
+        {
+            // Arrange
+            ListNode head = new ListNode(1);
+            head.next = new ListNode(1);
+
+            ListNode expected = new ListNode(1);
+
+            // Act
+            var actual = _removeDuplicates.DeleteDuplicates(head);
+
+            // Assert
+            Assert.AreEqual(expected.val, actual.val);
+            Assert.IsNull(actual.next);
+        }
+
+        [TestMethod]
+        public void DeleteDuplicates_WhenGivenExampleOne_ReturnsExpectedList()
+        {
+            // Arrange
+            ListNode head = new ListNode(1);
+            head.next = new ListNode(1);
+            head.next.next = new ListNode(2);
+
+            ListNode expected = new ListNode(1);
+            expected.next = new ListNode(2);
+
+            // Act
+            var actual = _removeDuplicates.DeleteDuplicates(head);
+
+            // Assert
+            Assert.AreEqual(expected.val, actual.val);
+            Assert.AreEqual(expected.next.val, actual.next.val);
+        }
+
+        [TestMethod]
+        public void DeleteDuplicates_WhenGivenListWithThreeDuplicateValues_ReturnsSingleNode()
+        {
+            // Arrange
+            ListNode head = new ListNode(1);
+            head.next = new ListNode(1);
+            head.next.next = new ListNode(1);
+
+            ListNode expected = new ListNode(1);
+
+            // Act
+            var actual = _removeDuplicates.DeleteDuplicates(head);
+
+            // Assert
+            Assert.AreEqual(expected.val, actual.val);
+            Assert.IsNull(actual.next);
+        }
+    }
+}
---
+++ LeetCode/Solutions/Easy/RemoveDuplicates.cs
@@ -0,0 +1,55 @@
+﻿using Utility;
+
+namespace Solutions.Easy
+{
+    /// <summary>
+    /// A class for solving leetcode problem 83. Remove Duplicates from Sorted List
+    /// </summary>
+    /// Given the head of a sorted linked list, delete all duplicates such that each element appears only once. Return the linked list sorted as well.
+    /// 
+    /// Example 1:
+    /// Input: head = [1,1,2]
+    /// Output: [1,2]
+    /// 
+    /// Example 2:
+    /// Input: head = [1,1,2,3,3]
+    /// Output: [1,2,3]
+    /// 
+    /// Constraints:
+    /// The number of nodes in the list is in the range[0, 300].
+    /// -100 <= Node.val <= 100
+    /// The list is guaranteed to be sorted in ascending order.
+    public class RemoveDuplicates
+    {
+        public ListNode DeleteDuplicates(ListNode head)
+        {
+            // Trivial cases
+            // Empty list
+            if (head == null) return null;
+
+            // Only one element in list
+            if (head.next == null) return head;
+
+            // So we want to traverse the list, and given it's sorted, we can just 
+            // remove any node who's value is the same as the previous node.
+            // We'll keep head separate for returning later.
+            ListNode currentNode = head;
+
+            while (currentNode.next is not null)
+            {
+                // If the next node has the same value, then we want to skip it, that is set currentNode.next 
+                // to the next.next node, if it exists, or null otherwise.
+                if (currentNode.next.val == currentNode.val)
+                {
+                    currentNode.next = currentNode.next.next;
+                }
+                else
+                {
+                    currentNode = currentNode.next is not null ? currentNode.next : currentNode;
+                }
+            }
+
+            return head;
+        }
+    }
+}

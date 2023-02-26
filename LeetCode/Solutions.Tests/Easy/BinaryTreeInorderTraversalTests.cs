98a20bd69a88eac840cfa39eb2ec714765b74d73
Thomas <thombsaway@gmail.com>
2023-02-26 16:47:06 +1100

    Add BinaryTreeInorderTraversal and initial tests.

---
+++ LeetCode/Solutions.Tests/Easy/BinaryTreeInorderTraversalTests.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Solutions.Easy;
+
+namespace Solutions.Tests.Easy
+{
+    [TestClass]
+    public class BinaryTreeInorderTraversalTests
+    {
+        private readonly BinaryTreeInorderTraversal _binaryTreeInorderTraversal;
+        public BinaryTreeInorderTraversalTests()
+        {
+            _binaryTreeInorderTraversal = new BinaryTreeInorderTraversal();
+        }
+
+        [TestMethod]
+        public void InorderTraversal_WhenPassedEmptyNode_ReturnsEmptyNode()
+        {
+            // Arrange
+            TreeNode input = null;
+
+            // Act
+            var actual = _binaryTreeInorderTraversal.InorderTraversal(input);
+
+            // Assert
+            Assert.IsNull(actual);
+        }
+
+        [TestMethod]
+        public void InorderTraversal_WhenPassedNonEmptyNode_ReturnsNonEmptyNode()
+        {
+            // Arrange
+            TreeNode input = new TreeNode();
+
+            // Act
+            var actual = _binaryTreeInorderTraversal.InorderTraversal(input);
+
+            // Assert
+            Assert.IsNotNull(actual);
+        }
+    }
+}
---
+++ LeetCode/Solutions/Easy/BinaryTreeInorderTraversal.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Solutions.Easy
+{
+    /// <summary>
+    /// A class to solve Leetcode problem 94. Binary Tree Inorder Traversal
+    /// </summary>
+    /// Given the root of a binary tree, return the inorder traversal of its nodes' values.
+    /// 
+    /// Example 1:
+    /// Input: root = [1,null,2,3]
+    /// Output: [1,3,2]
+    /// 
+    /// Example 2:
+    /// Input: root = []
+    /// Output: []
+    /// 
+    /// Example 3:
+    /// Input: root = [1]
+    /// Output: [1]
+    /// 
+    /// Constraints:
+    /// The number of nodes in the tree is in the range[0, 100].
+    /// -100 <= Node.val <= 100
+    /// 
+    /// Follow up: Recursive solution is trivial, could you do it iteratively?
+    public class BinaryTreeInorderTraversal
+    {
+        public IList<int> InorderTraversal(TreeNode root)
+        {
+            List<int> result = new List<int>();
+
+            while(root != null)
+            {
+                root = root.left;
+            }
+
+            result.Add(root.val);
+
+            return result;
+        }
+    }
+
+    public class TreeNode
+    {
+        public int val;
+        public TreeNode left;
+        public TreeNode right;
+
+        public TreeNode(int val = 0, TreeNode left = null, TreeNode right = null)
+        {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+}

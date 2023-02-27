e3a35547d9c130e2a7d692a948932c5a9129d4a5
Thomas <thombsaway@gmail.com>
2023-02-27 13:29:27 +1100

    Refactor into BinaryTree class, there are a few LeetCode problems using this class.

---
+++ LeetCode/Solutions.Tests/Easy/BinaryTreeTests.cs
@@ -0,0 +1,122 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Solutions.Easy;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Solutions.Tests.Easy
+{
+    [TestClass]
+    public class BinaryTreeTests
+    {
+        private readonly BinaryTree _binaryTree;
+        public BinaryTreeTests()
+        {
+            _binaryTree = new BinaryTree();
+        }
+
+        [TestMethod]
+        public void InorderTraversal_WhenPassedEmptyNode_ReturnsEmptyNode()
+        {
+            // Arrange
+            TreeNode input = null;
+
+            // Act
+            var actual = _binaryTree.InorderTraversal(input);
+
+            // Assert
+            Assert.AreEqual(actual.Count, 0);
+        }
+
+        [TestMethod]
+        public void InorderTraversal_WhenPassedNonEmptyNode_ReturnsNonEmptyNode()
+        {
+            // Arrange
+            TreeNode input = new TreeNode();
+
+            // Act
+            var actual = _binaryTree.InorderTraversal(input);
+
+            // Assert
+            Assert.IsNotNull(actual);
+        }
+
+        [TestMethod]
+        public void InOrderTraversal_WhenPassedExampleOne_ReturnsExpectedResult()
+        {
+            // Arrange
+            TreeNode input = new TreeNode();
+            input.val = 1;
+            input.left = new TreeNode()
+            {
+                val = 3,
+                left = new TreeNode()
+                {
+                    val = 2
+                },
+                right = null
+            };
+
+            IList<int> expected = new List<int>() { 1, 3, 2 };
+
+            // Act
+            var actual = _binaryTree.InorderTraversal(input);
+
+            // Assert
+            CollectionAssert.AreEquivalent(expected.ToArray(), actual.ToArray());
+        }
+
+        [TestMethod]
+        public void PreOrderTraversal_WhenPassedEmptyNode_ReturnsEmptyNode()
+        {
+            // Arrange
+            TreeNode input = null;
+
+            // Act
+            var actual = _binaryTree.PreOrderTraversal(input);
+
+            // Assert
+            Assert.AreEqual(actual.Count, 0);
+        }
+
+        [TestMethod]
+        public void PreOrderTraversal_WhenPassedNonEmptyNode_ReturnsNonEmptyNode()
+        {
+            // Arrange
+            TreeNode input = new TreeNode();
+
+            // Act
+            var actual = _binaryTree.PreOrderTraversal(input);
+
+            // Assert
+            Assert.IsNotNull(actual);
+        }
+
+        [TestMethod]
+        public void PreOrderTraversal_WhenPassedExampleOne_ReturnsExpectedResult()
+        {
+            // Arrange
+            TreeNode input = new TreeNode();
+            input.val = 1;
+            input.left = new TreeNode()
+            {
+                val = 3,
+                left = new TreeNode()
+                {
+                    val = 2
+                },
+                right = null
+            };
+
+            IList<int> expected = new List<int>() { 1, 2, 3 };
+
+            // Act
+            var actual = _binaryTree.PreOrderTraversal(input);
+
+            // Assert
+            CollectionAssert.AreEquivalent(expected.ToArray(), actual.ToArray());
+        }
+    }
+}
---
+++ LeetCode/Solutions/Easy/BinaryTree.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Solutions.Easy
+{
+    public class BinaryTree
+    {
+        private List<int> result;
+
+        #region InOrder
+        public IList<int> InorderTraversal(TreeNode root)
+        {
+            result = new List<int>();
+
+            InOrderRecursive(root);
+
+            return result;
+        }
+
+        public void InOrderRecursive(TreeNode root)
+        {
+            if (root == null)
+            {
+                return;
+            }
+
+            InOrderRecursive(root.left);
+            result.Add(root.val);
+            InOrderRecursive(root.right);
+        }
+        #endregion
+
+        #region PreOrder
+        /// <summary>
+        /// Returns a list of values from a binary tree, in pre order.
+        /// </summary>
+        /// <param name="root"></param>
+        /// <returns></returns>
+        public IList<int> PreOrderTraversal(TreeNode root)
+        {
+            result = new List<int>();
+
+            PreOrderRecursive(root);
+
+            return result;
+        }
+
+        public void PreOrderRecursive(TreeNode root)
+        {
+            if (root == null)
+            {
+                return;
+            }
+
+            result.Add(root.val);
+            
+            PreOrderRecursive(root.left);
+            
+            PreOrderRecursive(root.right);
+        }
+        #endregion
+    }
+}

228b339f89e5741157f4ae39769715f2039b375a
Thomas <thombsaway@gmail.com>
2023-03-17 11:22:00 +1100

    Stub out daily leetcode challenge, PrefixTree, or Trie.

---
+++ LeetCode/Solutions.Tests/Medium/PrefixTreeTests.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Solutions.Medium;
+namespace Solutions.Tests.Medium
+{
+    [TestClass]
+    public class PrefixTreeTests
+    {
+        [TestMethod]
+        public void Insert_WhenCalledWithString_InsertsStringIntoTrie()
+        {
+            // Arrange
+
+            // Act 
+
+            // Assert
+
+        }
+
+        [TestMethod]
+        public void Search_WhenCalledWithNonExistentString_ReturnsFalse()
+        {
+            // Arrange
+
+            // Act 
+
+            // Assert
+
+        }
+
+        [TestMethod]
+        public void Search_WhenCalledWithExistingString_ReturnsTrue()
+        {
+            // Arrange
+
+            // Act 
+
+            // Assert
+
+        }
+
+        [TestMethod]
+        public void StartsWith_WhenCalledWithNonExistentPrefix_ReturnsFalse()
+        {
+            // Arrange
+
+            // Act 
+
+            // Assert
+
+        }
+
+        [TestMethod]
+        public void StartsWith_WhenCalledWithExistingPrefix_ReturnsTrue()
+        {
+            // Arrange
+
+            // Act 
+
+            // Assert
+
+        }
+    }
+}
---
+++ LeetCode/Solutions/Medium/PrefixTree.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Solutions.Medium
+{
+    /// <summary>
+    /// A class for solving Leetcode problem 208. Implement Trie (Prefix Tree)
+    /// </summary>
+    /// A trie (pronounced as "try") or prefix tree is a tree data structure used to efficiently store and retrieve keys in a dataset of strings.
+    /// There are various applications of this data structure, such as autocomplete and spellchecker.
+    /// 
+    /// Implement the Trie class:
+    /// Trie() Initializes the trie object.
+    /// void insert(String word) Inserts the string word into the trie.
+    /// boolean search(String word) Returns true if the string word is in the trie (i.e., was inserted before), and false otherwise.
+    /// boolean startsWith(String prefix) Returns true if there is a previously inserted string word that has the prefix prefix, and false otherwise.
+    /// 
+    /// Example 1:
+    /// Input ["Trie", "insert", "search", "search", "startsWith", "insert", "search"]
+    /// [[], ["apple"], ["apple"], ["app"], ["app"], ["app"], ["app"]]
+    /// 
+    /// Output
+    /// [null, null, true, false, true, null, true]
+    /// 
+    /// Explanation
+    /// Trie trie = new Trie();
+    /// trie.insert("apple");
+    /// trie.search("apple");   // return True
+    /// trie.search("app");     // return False
+    /// trie.startsWith("app"); // return True
+    /// trie.insert("app");
+    /// trie.search("app");     // return True
+    /// 
+    /// Constraints:
+    /// 
+    /// 1 <= word.length, prefix.length <= 2000
+    /// word and prefix consist only of lowercase English letters.
+    /// At most 3 * 104 calls in total will be made to insert, search, and startsWith.
+    public class PrefixTree
+    {
+        public PrefixTree()
+        {
+
+        }
+
+        /// <summary>
+        /// Inserts the given word into the Trie.
+        /// </summary>
+        /// <param name="word"></param>
+        public void Insert(string word)
+        {
+
+        }
+
+        /// <summary>
+        /// Returns true if the given word is in the Trie already.
+        /// </summary>
+        /// <param name="word"></param>
+        /// <returns></returns>
+        public bool Search(string word)
+        {
+
+        }
+
+        /// <summary>
+        /// Returns true if there exists a word in the Trie which has the given prefix.
+        /// </summary>
+        /// <param name="prefix"></param>
+        /// <returns></returns>
+        public bool StartsWith(string prefix)
+        {
+
+        }
+    }
+}

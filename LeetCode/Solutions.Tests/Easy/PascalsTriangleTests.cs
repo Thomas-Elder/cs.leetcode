2a4f22e47535c11a2578090de3b6edd58a15a125
Thomas <thombsaway@gmail.com>
2023-03-22 11:51:33 +1100

    Implement PascalsTriangle solution.

---
+++ LeetCode/Solutions.Tests/Easy/PascalsTriangleTests.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Solutions.Easy;
+using System.Collections.Generic;
+
+namespace Solutions.Tests.Easy
+{
+    [TestClass]
+    public class PascalsTriangleTests
+    {
+        private readonly PascalsTriangle _pascalsTriangle;
+
+        public PascalsTriangleTests()
+        {
+            _pascalsTriangle = new PascalsTriangle();
+        }
+
+        [TestMethod]
+        public void Generate_WhenCalledWithExampleOne_ReturnsExpectedResult()
+        {
+            // Arrange
+            var input = 5;
+
+            IList<IList<int>> expected = new List<IList<int>>();
+            expected.Add(new List<int>() { 1 });
+            expected.Add(new List<int>() { 1, 1 });
+            expected.Add(new List<int>() { 1, 2, 1 });
+            expected.Add(new List<int>() { 1, 3, 3, 1 });
+            expected.Add(new List<int>() { 1, 4, 6, 4, 1});
+
+            // Act
+            var actual = _pascalsTriangle.Generate(input);
+
+            // Assert
+            Assert.AreEqual(expected[2][1], actual[2][1]);
+            Assert.AreEqual(expected[3][2], actual[3][2]);
+        }
+    }
+}
---
+++ LeetCode/Solutions/Easy/PascalsTriangle.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections.Generic;
+
+namespace Solutions.Easy
+{
+    public class PascalsTriangle
+    {
+        /// <summary>
+        /// Given an integer numRows, returns the first numRows of Pascal's triangle.
+        /// </summary>
+        /// <param name="numRows"></param>
+        /// <returns></returns>
+        public IList<IList<int>> Generate(int numRows)
+        {
+            IList<IList<int>> result = new List<IList<int>>();
+
+            for (int i = 0; i < numRows; i++)
+            {
+                var row = new List<int>();
+
+                // If i == 0, first row, is always just 1
+                if (i == 0)
+                {
+                    row.Add(1);
+                } else if (i == 1)
+                {
+                    row.Add(1);
+                    row.Add(1);
+                } else
+                {
+                    // Add first cell
+                    row.Add(1);
+
+                    for (int j = 1; j < i; j++)
+                    {
+                        // sum previous row's ints
+                        var sum = result[i - 1][j - 1] + result[i - 1][j];
+                        row.Add(sum);
+                    }
+
+                    // Add last cell
+                    row.Add(1);
+                }
+
+                result.Add(row);
+            }
+
+            return result;
+        }
+    }
+}

b93bcf6044fd99807d850b46427d2adeb9311f1e
Thomas <thombsaway@gmail.com>
2023-05-15 15:46:46 +1000

    ContainerWIthMostWater problem solution added.

---
+++ LeetCode/Solutions.Tests/Medium/ContainerWithMostWaterTests.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Solutions.Medium;
+
+namespace Solutions.Tests.Medium
+{
+    [TestClass]
+    public class ContainerWithMostWaterTests
+    {
+        private readonly ContainerWithMostWater _containerWithMostWater;
+
+        public ContainerWithMostWaterTests()
+        {
+            _containerWithMostWater = new ContainerWithMostWater();
+        }
+
+        [TestMethod]
+        public void ContainerWithMostWater_WhenCalledWithExampleOne_ReturnsExpectedAnswer()
+        {
+            // Arrange
+            var input = new int [] { 1, 8, 6, 2, 5, 4, 8, 3, 7 };
+            var expected = 49;
+
+            // Act
+            var actual = _containerWithMostWater.MaxArea(input);
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void ContainerWithMostWater_WhenCalledWithExampleTwo_ReturnsExpectedAnswer()
+        {
+            // Arrange
+            var input = new int[] { 1, 1 };
+            var expected = 1;
+
+            // Act
+            var actual = _containerWithMostWater.MaxArea(input);
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
---
+++ LeetCode/Solutions/Medium/ContainerWithMostWater.cs
@@ -0,0 +1,64 @@
+﻿using System;
+
+namespace Solutions.Medium
+{
+    /// <summary>
+    /// Class for solving leetcode problem 11. Container With Most Water
+    /// </summary>
+    /// You are given an integer array height of length n. There are n vertical lines drawn such that the two endpoints of the ith line are (i, 0) and (i, height[i]).
+    /// Find two lines that together with the x-axis form a container, such that the container contains the most water.
+    /// 
+    /// Return the maximum amount of water a container can store
+    /// 
+    /// Notice that you may not slant the container.
+    /// 
+    /// Example 1:
+    /// Input: height = [1, 8, 6, 2, 5, 4, 8, 3, 7]
+    /// Output: 49
+    /// Explanation: The above vertical lines are represented by array [1,8,6,2,5,4,8,3,7]. In this case, the max area of water (blue section) the container can contain is 49.
+    /// 
+    /// Example 2: 
+    /// Input: height = [1, 1]
+    /// Output: 1
+    /// 
+    /// Constraints:
+    /// n == height.length
+    /// 2 <= n <= 105
+    /// 0 <= height[i] <= 104
+    public class ContainerWithMostWater
+    {
+        public int MaxArea(int[] height)
+        {
+            // get the start and end indices
+            int left = 0;
+            int right = height.Length - 1;
+
+            // track maxArea
+            int maxArea = 0;
+            
+            while (left < right)
+            {
+                // find the area between the current indices
+                // save in maxArea if it's bigger than maxArea
+                // increment the index with the smaller height
+                var area = Math.Min(height[left], height[right]) * (right - left);
+
+                if (area > maxArea)
+                {
+                    maxArea = area;
+                }
+
+                // Advance whichever index has the lower value
+                if (height[left] < height[right])
+                {
+                    left++;
+                } else
+                {
+                    right--;
+                }
+            }
+
+            return maxArea;
+        }
+    }
+}

33a298d36fa18f792844c08e9a8535f580a6ef0d
Thomas <thombsaway@gmail.com>
2023-07-25 10:58:54 +1000

    IMplement and test leetcode problem 125. Valid Palindrome

---
+++ LeetCode/Solutions.Tests/Easy/ValidPalindromeTests.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Solutions.Easy;
+
+namespace Solutions.Tests.Easy
+{
+    [TestClass]
+    public class ValidPalindromeTests
+    {
+        private readonly ValidPalindrome _validPalindrome;
+
+        public ValidPalindromeTests()
+        {
+            _validPalindrome = new ValidPalindrome();
+        }
+
+        [TestMethod]
+        public void IsPalindrome_WhenCalledWithExampleOne_ReturnsExpectedResult()
+        {
+            // Arrange
+            var input = "A man, a plan, a canal: Panama";
+            var expected = true;
+
+            // Act
+            var actual = _validPalindrome.IsPalindrome(input);
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void IsPalindrome_WhenCalledWithExampleTwo_ReturnsExpectedResult()
+        {
+            // Arrange
+            var input = "race a car";
+            var expected = false;
+
+            // Act
+            var actual = _validPalindrome.IsPalindrome(input);
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void IsPalindrome_WhenCalledWithExampleThree_ReturnsExpectedResult()
+        {
+            // Arrange
+            var input = " ";
+            var expected = true;
+
+            // Act
+            var actual = _validPalindrome.IsPalindrome(input);
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
---
+++ LeetCode/Solutions/Easy/ValidPalindrome.cs
@@ -0,0 +1,67 @@
+﻿
+using System.Diagnostics;
+using System.Diagnostics.Metrics;
+using System.Text.RegularExpressions;
+
+namespace Solutions.Easy
+{
+    /// <summary>
+    /// A class for solving leetcode problem 125. Valid Palindrome
+    /// </summary>
+    /// A phrase is a palindrome if, after converting all uppercase letters into lowercase letters and removing all 
+    /// non-alphanumeric characters, it reads the same forward and backward. Alphanumeric characters include letters and numbers.
+    /// 
+    /// Given a string s, return true if it is a palindrome, or false otherwise.
+    /// 
+    /// Example 1:
+    /// Input: s = "A man, a plan, a canal: Panama"
+    /// Output: true
+    /// Explanation: "amanaplanacanalpanama" is a palindrome.
+    /// 
+    /// Example 2:
+    /// Input: s = "race a car"
+    /// Output: false
+    /// Explanation: "raceacar" is not a palindrome.
+    /// 
+    /// Example 3:
+    /// Input: s = " "
+    /// Output: true
+    /// Explanation: s is an empty string "" after removing non-alphanumeric characters.
+    /// Since an empty string reads the same forward and backward, it is a palindrome.
+    /// 
+    /// Constraints:
+    /// 1 <= s.length <= 2 * 105
+    /// s consists only of printable ASCII characters.
+    public class ValidPalindrome
+    {
+        public bool IsPalindrome(string s)
+        {
+            var result = false;
+
+            // Trivial case
+            if (s.Length == 0 || s.Length == 1) 
+            {
+                return true;
+            }
+
+            // strip and convert
+            string convertedString = Regex.Replace(s, @"[^a-zA-Z0-9]", "").ToLower();
+
+            var i = 0;
+            var j = convertedString.Length - 1;
+
+            while (i < j)
+            {
+                if (convertedString[i] != convertedString[j])
+                {
+                    return false;
+                }
+
+                i++;
+                j--;
+            }
+
+            return true;
+        }
+    }
+}
